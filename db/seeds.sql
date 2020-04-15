97268509e844404bc144ad0f2c1d96f156d24310
Kathleen Tonner <kathleentonner@gmail.com>
2020-04-15 18:13:58 -0400

    Refactoring

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Royale with Cheese', false);
+
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,22 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    console.log(id)
+    var newDevour = $(this).data("newdevour");
+    console.log(newDevour)
 
-    var newSleepState = {
-      sleepy: newSleep
+    var newDevourState = {
+      devoured: newDevour
     };
-
+    console.log(newDevourState)
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newDevourState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed Devour to", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -25,18 +27,18 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      devoured: 0
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
